6762d9e88738a501fe476c35f69295b06fb2808a
Sanjay S <sanjaypskumar@gmail.com>
2022-08-07 20:48:03 -0400

    Initial commit

---
+++ src/main/java/com/track/stocksapp/StocksappApplication.java
@@ -0,0 +1,13 @@
+package com.track.stocksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/track/stocksapp/StocksappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.track.stocksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
